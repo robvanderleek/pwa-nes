667dd4d2ad40ed140d3b99880a93e9e66cbddcf2
Rob van der Leek <robvanderleek@gmail.com>
2021-06-26 20:32:12 +0200

    Testing...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {useState} from "react";
 
 const Main = styled.div`
     height: 100%;
@@ -8,21 +9,36 @@ const Main = styled.div`
 `;
 
 const Area = styled.div`
+    height: 100%;
     flex-grow: 1;
     display: flex;
+    flex-direction: column;
     justify-content: center;
+    align-items: center;
 `;
 
 export default function App() {
+    const [messages, setMessages] = useState([]);
+
+    function pushLeft() {
+        messages.push('left');
+        setMessages([...messages]);
+    }
+
+    function pushRight() {
+        messages.push('right');
+        setMessages([...messages]);
+    }
+
     return (
         <Main>
-            <Area>
+            <Area onClick={pushLeft}>
                 <div>left</div>
             </Area>
             <Area>
-                <div>middle</div>
+                {messages.map((m, i) => <div key={i}>{m}</div>)}
             </Area>
-            <Area>
+            <Area onClick={pushRight}>
                 <div>right</div>
             </Area>
         </Main>
