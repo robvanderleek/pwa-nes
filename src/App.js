7c09c96f1fb9699016735e69307f70efc6d8a1bd
Rob van der Leek <robvanderleek@gmail.com>
2021-06-26 18:54:02 +0200

    Left, middle, and right

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Main = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+`;
+
+const Area = styled.div`
+    flex-grow: 1;
+    display: flex;
+    justify-content: center;
+`;
+
+export default function App() {
+    return (
+        <Main>
+            <Area>
+                <div>left</div>
+            </Area>
+            <Area>
+                <div>middle</div>
+            </Area>
+            <Area>
+                <div>right</div>
+            </Area>
+        </Main>
+    );
 }
-
-export default App;
