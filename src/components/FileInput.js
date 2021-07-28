38cfb8666a0f9a3b0f6c02e805eef1a695e467b0
Rob van der Leek <robvanderleek@gmail.com>
2021-07-28 23:50:20 +0200

    All files

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -36,7 +36,7 @@ export default function FileInput(props) {
                            onTouchEnd={() => onUp && onUp(title)}
         >
             <span>{title}</span>
-            <input type="file" accept="application/pdf"
+            <input type="file" accept="all/allfiles"
                    onChange={handleOnChange}/>
         </NoUserSelectLabel>
     );
