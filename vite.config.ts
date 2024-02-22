3c0c12f21082ff04b23182f0ab0e9f06d187015b
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-02-22 15:02:40 +0100

    Migrating away from CRA...

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
+import fs from 'fs/promises';
+
+export default defineConfig({
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: "load-js-files-as-jsx",
+          setup(build) {
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+              loader: "jsx",
+              contents: await fs.readFile(args.path, "utf8"),
+            }));
+          },
+        },
+      ],
+    },
+  },
+  assetsInclude: ['**/*.zip'],
+  plugins: [
+    react(), 
+    viteTsconfigPaths(),
+    svgr({
+      include: '**/*.svg?react',
+    }),
+  ],
+});
